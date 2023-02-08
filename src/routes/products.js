99cb1bc3e327b53b283e8fc4426cb8f785e5e194
RamonBritez <rbritez966@gmail.com>
2023-02-08 18:21:29 -0300

    Tp 6

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,20 +9,20 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
-router.get('/create/', productsController.create); 
-router.post('/', productsController.store); 
+//router.get('/create/', productsController.create); 
+//router.post('/', productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
 router.get('/detail/:id/', productsController.detail); 
 
 /*** EDIT ONE PRODUCT ***/ 
-router.get('/edit/:id', productsController.edit); 
-router.put('/edit/:id', productsController.update); 
+//router.get('/edit/:id', productsController.edit); 
+//router.put('/edit/:id', productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
-router.delete('/delete/:id', productsController.destroy); 
+//router.delete('/delete/:id', productsController.destroy); 
 
 
 module.exports = router;
